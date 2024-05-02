7c2b965e55332e17aa80663b1c351dc1f924b7cd
yjmalulu <yjmalulu@gmail.com>
2024-05-02 11:47:18 -0700

    add files

--- crypto_wallet.py
+++ crypto_wallet.py
@@ -69,7 +69,7 @@ def send_transaction(w3, account, to, wage):
         "from": account.address,
         "value": value,
         "gas": gasEstimate,
-        "gasPrice": 0,
+        "gasPrice": 9000000000,
         "nonce": w3.eth.getTransactionCount(account.address),
     }
 
